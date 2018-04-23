3e31c51700729d4bb3afd89c74699477de61d6e6
Keiichiro HITOMI <kei.hito17@gmail.com>
2018-04-23 21:06:11 +0900

    正規表現に対応

--- src/extension.ts
+++ src/extension.ts
@@ -1,58 +1,35 @@
 'use strict';
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
+
 import * as vscode from 'vscode';
 
-// this method is called when your extension is activated
-// your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
 
-    console.log('Congratulations, your extension "grep" is now active!');
-    let disposable = vscode.commands.registerCommand('extension.sayHello', () => {
-        vscode.window.showInformationMessage('Hello World!');
-    
-
-        var prompt = vscode.window.showInputBox();
-        prompt.then((value) => {
-            if (value === undefined) {
-                return;
-            }
-            let editor = vscode.window.activeTextEditor; // エディタ取得
-    
+    let grepCommand = vscode.commands.registerCommand('extension.grep', () => {
+        var prompt = vscode.window.showInputBox({prompt: 'Please type a search word.'});
+        prompt.then((word) => {
+            let editor = vscode.window.activeTextEditor;
             if (!editor) {
-                vscode.window.showErrorMessage("File is not opened. Please open a file.");
+                vscode.window.showErrorMessage("A target file is not opened. Please open a target file.");
                 return;
             }
 
-            let doc = editor.document;            // ドキュメント取得
-            let cur_selection = editor.selection; // 選択範囲取得
-        
-            if(editor.selection.isEmpty){         
-                let startPos = new vscode.Position(0, 0);
-                let endPos = new vscode.Position(doc.lineCount - 1, 10000);
-                cur_selection = new vscode.Selection(startPos, endPos);
-            }
-    
-            let text = doc.getText(cur_selection); //取得されたテキスト
-            var lines = text.split(/\r\n|\r|\n/);
-            var result = "";
-
-            for (var i = 0; i < lines.length; i++) {
-                if (lines[i].includes(value)) {
-                    result = result + lines[i] + '\r\n';
-                }
+            if (!word) {
+                vscode.window.showErrorMessage("Please type search word.");
+                return;
             }
 
+            let text = getSelectionText(editor);
             vscode.workspace.openTextDocument().then(
                 doc => {
                     vscode.window.showTextDocument(doc).then(
                         doc => {
                             let e = vscode.window.activeTextEditor;
                             if (!e) {
+                                vscode.window.showErrorMessage("unexpected error[0001].");
                                 return;
                             } 
                             e.edit(edit => {
-                                edit.insert(new vscode.Position(0,0), result);
+                                edit.insert(new vscode.Position(0,0), getGrepTextOf(text, word));
                             });
                         }
                     );
@@ -61,9 +38,77 @@ export function activate(context: vscode.ExtensionContext) {
         });
     });
 
-    context.subscriptions.push(disposable);
+    let grepexCommand = vscode.commands.registerCommand('extension.grepex', () => {
+        var prompt = vscode.window.showInputBox({prompt: 'Please type a search Regex Pattern.'});
+        prompt.then((word) => {
+            let editor = vscode.window.activeTextEditor;
+            if (!editor) {
+                vscode.window.showErrorMessage("A target file is not opened. Please open a target file.");
+                return;
+            }
+
+            if (!word) {
+                vscode.window.showErrorMessage("Please type search word.");
+                return;
+            }
+
+            let text = getSelectionText(editor);
+            vscode.workspace.openTextDocument().then(
+                doc => {
+                    vscode.window.showTextDocument(doc).then(
+                        doc => {
+                            let e = vscode.window.activeTextEditor;
+                            if (!e) {
+                                vscode.window.showErrorMessage("unexpected error[0002].");
+                                return;
+                            } 
+                            e.edit(edit => {
+                                edit.insert(new vscode.Position(0,0), getGrepTextRegOf(text, word));
+                            });
+                        }
+                    );
+                }
+            );
+        });
+    });
+
+    context.subscriptions.push(grepCommand);
+    context.subscriptions.push(grepexCommand);
 }
 
+function getSelectionText(editor: vscode.TextEditor): string {
+    let doc = editor.document;
+    let cur_selection = editor.selection;
+    if(editor.selection.isEmpty){         
+        let startPos = new vscode.Position(0, 0);
+        let endPos = new vscode.Position(doc.lineCount - 1, 1000000);
+        cur_selection = new vscode.Selection(startPos, endPos);
+    }
+    return doc.getText(cur_selection);
+}
+
+function getGrepTextOf(text: string, word: string): string {
+    let lines = text.split(/\r\n|\r|\n/);
+    let result = "";
+    for (var i = 0; i < lines.length; i++) {
+        if (lines[i].includes(word)) {
+            result = result + lines[i] + '\r\n';
+        }
+    }
+    return result;
+}
+
+function getGrepTextRegOf(text: string, pattern: string): string {
+    let lines = text.split(/\r\n|\r|\n/);
+    let result = "";
+    var myregex = new RegExp(pattern);
+    for (var i = 0; i < lines.length; i++) {
+        if (lines[i].match(myregex)) {
+            result = result + lines[i] + '\r\n';
+        }
+    }
+    return result;
+}
 // this method is called when your extension is deactivated
 export function deactivate() {
 }
